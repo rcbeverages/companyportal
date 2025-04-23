47e468811eed30e7eec96d4bcde61cb9b463e4f9
rcbeverages <ryan@rcbeverages.com.au>
2025-04-23 21:52:35 +1000

    Delete header.js

--- header.js
+++
@@ -1,5 +0,0 @@
-// Inject dynamic header text based on the page
-document.addEventListener("DOMContentLoaded", function() {
-    const pageTitle = document.title; // Get the title of the page
-    document.getElementById("page-title").textContent = pageTitle; // Set it to the header
-});
